aea0b756ea480d3bc99a4ec56cd20b5bb6bdc72f
Mr. Bird <pc.racidae@gmail.com>
2026-02-17 10:32:16 +0800

    Added sprite folder.

--- script/furniture.gd
+++ script/furniture.gd
@@ -1,6 +1,9 @@
 extends Node2D
 class_name Furniture
 
+# SIZE: SML (32x32) MED (48x32) LRG (64x32)
+# TYPE
+
 func _init(x: int, y: int):
 	
 	self.x = x;
@@ -10,7 +13,9 @@ func _init(x: int, y: int):
 
 func move(x: int, y: int):
 	
-	self.y += (x - y) * 8;		# Change later for autoload.
+	# Change later for autoload.
+	
+	self.y += (x - y) * 8;
 	self.x += (x + y) * 16;
 	
 	return;
