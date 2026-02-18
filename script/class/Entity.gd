6221d1fefe16660889557887b9074b70f4f766bb
Mr. Bird <pc.racidae@gmail.com>
2026-02-19 06:20:22 +0800

    Fixed '_init()'

--- script/class/Entity.gd
+++ script/class/Entity.gd
@@ -10,12 +10,16 @@ var health: int = MAX_HEALTH # Internal use only.
 func getHp() -> int:
 	return health
 
-func _init(hp: int, spd: int) -> void:
-	MAX_HEALTH = hp
-	MOVE_SPEED = spd
+func _init(hp: int = 1, spd: int = 5) -> void:		# WARNING: _init is REQUIRED to either have
+	MAX_HEALTH = hp									# a PARAMETERLESS form or DEFAULT values!!!
+	MOVE_SPEED = spd								# It'll break if it doesn't
 	
 func hurt(amount: int) -> void:
 	health -= amount
 	if (health <= 0):
 		queue_free()
 		return
+		
+# func _physics_process(delta: float) -> void:		# TODO: Friction
+	
+	# move_and_slide()
