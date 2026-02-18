ffb315b5a511da639915b890e9eab2215efef9dd
Mr. Bird <pc.racidae@gmail.com>
2026-02-19 07:08:42 +0800

    Snake-like movement. Dirty code.

--- script/autoload/GAME.gd
+++ script/autoload/GAME.gd
@@ -1,3 +1,4 @@
 extends Node
 
 @onready var TILE_SIZE: Vector2 = Vector2(16, 16)
+@onready var FRAMERATE: int = 60
--- script/class/entity/player.gd
+++ script/class/entity/player.gd
@@ -2,20 +2,29 @@ extends Entity
 class_name Player
 
 @onready var SPRITE = $Sprite2D
+var direction: Vector2 = Vector2(0, 0) 
 func _process(delta: float) -> void:
-	SPRITE.position = position
+	SPRITE.position = (SPRITE.position + (position - SPRITE.position) * (1 - pow(.5, delta * GAME.FRAMERATE)))
+
+var time: float = 0.0
+func _physics_process(_delta: float) -> void:
+	
+	time += _delta
+	if (time >= .1):
+		time = 0
+		_move(direction)
 	
 	if (Input.is_action_just_pressed("UP") && !$UP.is_colliding()):
-		_move(Vector2(0, -1))
+		direction = Vector2(0, -1)
 		
 	if (Input.is_action_just_pressed("DOWN") && !$DOWN.is_colliding()):
-		_move(Vector2(0, 1))
+		direction = Vector2(0, 1)
 		
 	if (Input.is_action_just_pressed("LEFT") && !$LEFT.is_colliding()):
-		_move(Vector2(-1, 0))
+		direction = Vector2(-1, 0)
 		
 	if (Input.is_action_just_pressed("RIGHT") && !$RIGHT.is_colliding()):
-		_move(Vector2(1, 0))
+		direction = Vector2(1, 0)
 		
 func _move(dir: Vector2):
 	global_position += dir * GAME.TILE_SIZE
