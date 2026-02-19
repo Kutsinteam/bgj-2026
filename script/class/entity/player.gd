bc70f60a885dda53200f7202e333a24b900787c8
quackersjj <jacobjose0925@gmail.com>
2026-02-19 12:40:02 +0800

    delete unnecessary files add tilemap layer with temp tileset change player scene

--- script/class/entity/player.gd
+++ script/class/entity/player.gd
@@ -2,32 +2,41 @@ extends Entity
 class_name Player
 
 @onready var SPRITE = $Sprite2D
-var direction: Vector2 = Vector2(0, 0) 
+@export var MOVESPEED_MODIFIER = 0.5
+var direction = Vector2.RIGHT
 func _process(delta: float) -> void:
-	SPRITE.position = (SPRITE.position + (position - SPRITE.position) * (1 - pow(.5, delta * GAME.FRAMERATE)))
+	SPRITE.position = (SPRITE.position + (position - SPRITE.position) * (1 - pow(MOVESPEED_MODIFIER, delta * GAME.FRAMERATE)))
 
 var time: float = 0.0
 func _physics_process(_delta: float) -> void:
 	
 	time += _delta
-	if (time >= .5):
+	if (time >= MOVESPEED_MODIFIER/2):
 		_move(direction)
 	
 	if (Input.is_action_just_pressed("UP") && !$UP.is_colliding()):
-		direction = Vector2(0, -1)
-		_move(direction)
+		var newdir: Vector2 = Vector2(0, -1)
+		if (direction != newdir):
+			direction = newdir
+			_move(direction)
 		
 	if (Input.is_action_just_pressed("DOWN") && !$DOWN.is_colliding()):
-		direction = Vector2(0, 1)
-		_move(direction)
+		var newdir: Vector2 = Vector2(0, 1)
+		if (direction != newdir):
+			direction = newdir
+			_move(direction)
 		
 	if (Input.is_action_just_pressed("LEFT") && !$LEFT.is_colliding()):
-		direction = Vector2(-1, 0)
-		_move(direction)
+		var newdir: Vector2 = Vector2(-1, 0)
+		if (direction != newdir):
+			direction = newdir
+			_move(direction)
 		
 	if (Input.is_action_just_pressed("RIGHT") && !$RIGHT.is_colliding()):
-		direction = Vector2(1, 0)
-		_move(direction)
+		var newdir: Vector2 = Vector2(1, 0)
+		if (direction != newdir):
+			direction = newdir
+			_move(direction)
 		
 func _move(dir: Vector2):
 	time = 0 # Reset
