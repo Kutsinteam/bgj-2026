e00a8108e5e327d59980f402f2f8817b2b438e8c
santinod-dev <santinodadulla22@gmail.com>
2026-02-20 11:48:11 +0800

    feat: dash cooldown

--- script/class/main/Player.gd
+++ script/class/main/Player.gd
@@ -4,15 +4,19 @@ class_name Player
 var direction = Vector2.RIGHT
 var newdir: Vector2
 var time: float = 0.0
-
+var canDash = true
 func _move(dir: Vector2):
 	time = 0 # Reset
 	global_position += dir * GAME.TILE_SIZE
 
 func _dash(dir: Vector2): 
-	if (Input.is_action_just_pressed("DASH") && (!$UP.is_colliding() && !$DOWN.is_colliding() && 
-		!$LEFT.is_colliding() && !$RIGHT.is_colliding())): # need to improve dash limitation to direction
-		global_position += 3 * dir * GAME.TILE_SIZE
+		if(canDash == true):
+			
+			if (Input.is_action_just_pressed("DASH") && (!$UP.is_colliding() && !$DOWN.is_colliding() && 
+				!$LEFT.is_colliding() && !$RIGHT.is_colliding())): # need to improve dash limitation to direction
+				global_position += 3 * dir * GAME.TILE_SIZE
+				canDash = false
+		
 
 func _physics_process(_delta: float) -> void:
 	
@@ -48,3 +52,7 @@ func _process(delta: float) -> void:
 	# Movement and Movement Smoothening
 	SPRITE.position = (SPRITE.position + (position - SPRITE.position) * (1 - pow(0.5, delta * GAME.FRAMERATE)))
 	
+
+#  dash cooldown
+func _on_timer_timeout() -> void:
+	canDash = true
