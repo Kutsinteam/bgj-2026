333766b7afc1dd1a298619d6f0fc36116f768cf8
Jack Andie Tucay <tucayjackandie@gmail.com>
2026-02-21 21:19:43 +0800

    fix movement and dash readability

--- script/class/main/Player.gd
+++ script/class/main/Player.gd
@@ -6,17 +6,40 @@ var newdir: Vector2
 var time: float = 0.0
 var canDash = true
 var isMoving = false
+@onready var input_map = { # Makes the fucking input readable
+	"UP" : {
+		"direction": Vector2.UP,
+		"raycast": $UP
+	},
+	"DOWN" : {
+		"direction": Vector2.DOWN,
+		"raycast": $DOWN
+	},
+	"LEFT" : {
+		"direction": Vector2.LEFT,
+		"raycast": $LEFT
+	},
+	"RIGHT" : {
+		"direction": Vector2.RIGHT,
+		"raycast": $RIGHT
+	}}
+
 func _move(dir: Vector2):
 	time = 0 # Reset
 	global_position += dir * GAME.TILE_SIZE
 
 func _dash(dir: Vector2): 
-		if(canDash == true):
-			
-			if (Input.is_action_just_pressed("DASH") && (!$UP.is_colliding() && !$DOWN.is_colliding() && 
-				!$LEFT.is_colliding() && !$RIGHT.is_colliding())): # need to improve dash limitation to direction
-				global_position += 3 * dir * GAME.TILE_SIZE
-				canDash = false
+	var colliding = false
+	for input in input_map:
+		var action = input_map[input]
+		
+		if(action["raycast"].is_colliding()):
+			colliding = true
+	
+	if(canDash == true):
+		if (Input.is_action_just_pressed("DASH") and not colliding): # need to improve dash limitation to direction
+			global_position += 3 * dir * GAME.TILE_SIZE
+			canDash = false
 		
 
 func _physics_process(_delta: float) -> void:
@@ -27,37 +50,21 @@ func _physics_process(_delta: float) -> void:
 		_move(direction)
 	
 	# Movement
-	if (Input.is_action_just_pressed("UP") && direction != Vector2.UP && !$UP.is_colliding() 
-	&& isMoving == false):
-		direction = Vector2.UP
-		await get_tree().create_timer(0.2).timeout
-		isMoving = false
-			
-	elif (Input.is_action_just_pressed("DOWN") && direction != Vector2.DOWN && !$DOWN.is_colliding()
-	 && isMoving == false):
-		direction = Vector2.DOWN
-		await get_tree().create_timer(0.2).timeout
-		isMoving = false
-			
-	elif (Input.is_action_just_pressed("LEFT") && direction != Vector2.LEFT && !$LEFT.is_colliding() 
-	&& isMoving == false):
-		direction = Vector2.LEFT
-		await get_tree().create_timer(0.2).timeout
-		isMoving = false
-			
-	elif (Input.is_action_just_pressed("RIGHT") && direction != Vector2.RIGHT && !$RIGHT.is_colliding()
-	&& isMoving == false):
-		direction = Vector2.RIGHT
-		isMoving = true
-		await get_tree().create_timer(0.2).timeout
-		isMoving = false
-	_dash(direction)
-	
-	
+	for input in input_map:
+		var action = input_map[input]
+		
+		if(not isMoving):
+			if(Input.is_action_pressed(input) and direction != action["direction"] and not action["raycast"].is_colliding()):
+				direction = action["direction"]
+				isMoving = true
+				await get_tree().create_timer(0.2).timeout
+				isMoving = false
+				break
+		_dash(direction)
+
 func _process(delta: float) -> void:
 	# Movement and Movement Smoothening
 	SPRITE.position = (SPRITE.position + (position - SPRITE.position) * (1 - pow(0.5, delta * GAME.FRAMERATE)))
-	
 
 #  dash cooldown
 func _on_timer_timeout() -> void:
